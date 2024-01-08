2ecbedd721c676f521a76cf4ab5d7b2633c2fca0
Zareel Kalam <zareelbwd@gmail.com>
2024-01-08 20:14:30 +0530

    completed more taks

---
+++ 14divide.js
@@ -0,0 +1,4 @@
+//* Divide One Number by Another with JavaScript
+//? Divide One Number by Another with JavaScript
+
+const quotient = 66 / 33;
---
+++ 18multiplyTwoDecimals.js
@@ -0,0 +1,5 @@
+//* Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+//? Change the 0.0 so that product will equal 5.0.
+
+const product = 2.0 * 2.5;
