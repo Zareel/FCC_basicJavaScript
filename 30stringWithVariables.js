fc1dcf5ee39a641d36e2c7eb95ca4242010ec93f
Zareel Kalam <zareelbwd@gmail.com>
2024-01-09 14:25:37 +0530

    completed more tasks

---
+++ 30stringWithVariables.js
@@ -0,0 +1,8 @@
+//* Constructing Strings with Variables
+
+//? Set myName to a string equal to your name and build myStr with myName between the strings My name is  and  and I am well!
+
+// Only change code below this line
+const myName = "Zareel";
+const myStr = "My name is " + myName + "and I am well!";
+console.log(myStr);
