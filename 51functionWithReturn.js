2599ac4bf75edf3958ba470e8f7226f9e6e85bbf
Zareel Kalam <zareelbwd@gmail.com>
2024-01-12 12:50:07 +0530

    completed more tasks

---
+++ 51functionWithReturn.js
@@ -0,0 +1,9 @@
+//* Return a Value from a Function with Return
+
+//? Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(num) {
+  return num * 5;
+}
+let answer = timesFive(3);
+console.log(answer);
